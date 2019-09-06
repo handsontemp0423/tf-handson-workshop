7ec1e80613f25ce71a8b866e4c23f2d15a2b0dbd
handsontemp0423 <49933689+handsontemp0423@users.noreply.github.com>
2019-09-06 17:33:12 +0900

    oci terraform sample

--- main.tf
+++ main.tf
@@ -1,20 +1,26 @@
-terraform {
-required_version = " 0.12.2"
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "private_key" {}
+variable "region" {}
+
+
+# Configure the Oracle Cloud Infrastructure provider with an API Key
+provider "oci" {
+  tenancy_ocid = "${var.tenancy_ocid}"
+  user_ocid = "${var.user_ocid}"
+  fingerprint = "${var.fingerprint}"
+  private_key = "${var.private_key}"
+  region = "${var.region}"
 }
 
-provider "aws" {
-access_key = var.access_key
-secret_key = var.secret_key
-region = var.region
+# Get a list of Availability Domains
+data "oci_identity_availability_domains" "ads" {
+  compartment_id = "${var.tenancy_ocid}"
 }
 
-resource "aws_instance" "hello-tf-instance" {
-ami = var.ami
-count = var.hello_tf_instance_count
-instance_type = var.hello_tf_instance_type
-  tags = map(
-  "owner", "Kabu",
-  "ttl", "100"
-  )
-  
+# Output the result
+output "show-ads" {
+  value = "${data.oci_identity_availability_domains.ads.availability_domains}"
 }
+
